e88ceeae71f50f827f25170e9aa15a16ea857a7c
Yan Bisyukov <yanbisyukov@gmail.com>
2024-02-17 17:35:51 +0300

    read file into string, parse into hashmap, create profile

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file.getAbsolutePath()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line);
+                content.append(" ");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        String[] splitContent = content.toString().trim().split(" ");
+
+        HashMap<String, String> profileData = new HashMap<>();
+
+        for (int i = 0; i < splitContent.length; i += 2) {
+            String key = splitContent[i].replace(":", "");
+            String value = splitContent[i + 1];
+            profileData.put(key, value);
+        }
+
+        return new Profile(profileData.get("Name"), Integer.valueOf(profileData.get("Age")),
+                profileData.get("Email"), Long.valueOf(profileData.get("Phone"))
+        );
     }
 }
